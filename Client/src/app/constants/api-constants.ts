083d4f8eb6c45465cfd2b5381653182939a65035
sriram kumar mannava <sriramkumar.m@hotmail.com>
2020-05-25 12:35:21 +0530

    first commit

---
+++ Client/src/app/constants/api-constants.ts
@@ -0,0 +1,3 @@
+export class ApiConstants {
+    public static uri: string = "/api";
+}
---
+++ Client/src/app/fetch-data/fetch-data.component.ts
@@ -0,0 +1,24 @@
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ApiConstants } from '../constants/api-constants';
+
+@Component({
+  selector: 'app-fetch-data',
+  templateUrl: './fetch-data.component.html'
+})
+export class FetchDataComponent {
+  public forecasts: WeatherForecast[];
+
+  constructor(http: HttpClient) {
+    http.get<WeatherForecast[]>(ApiConstants.uri + '/weatherforecast').subscribe(result => {
+      this.forecasts = result;
+    }, error => console.error(error));
+  }
+}
+
+interface WeatherForecast {
+  date: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
+}
