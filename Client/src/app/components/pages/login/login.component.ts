31598fe49ea2eca2f68381119cf96db008de90a5
sriram kumar mannava <sriramkumar.m@hotmail.com>
2021-12-13 13:27:52 +0530

    Error handling on Login page. Moved AppBaseUrls to environment variables.

--- Client/src/app/components/pages/login/login.component.ts
+++ Client/src/app/components/pages/login/login.component.ts
@@ -3,6 +3,8 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 import { Router } from '@angular/router';
 import { LoginDTO } from 'src/app/models/LoginDTO';
 import { AuthService } from 'src/app/providers/auth.service';
+import { Location } from '@angular/common';
+import { TokenService } from 'src/app/providers/token.service';
 
 @Component({
   selector: 'app-login',
@@ -11,21 +13,44 @@ import { AuthService } from 'src/app/providers/auth.service';
 })
 export class LoginComponent implements OnInit {
   form: FormGroup;
+  isError: boolean = false;
+  errMsg: string = "Some error has occured!";
 
-  constructor(private formBuilder: FormBuilder, private authService: AuthService, private router: Router) {
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
+    private location: Location, private tokenService: TokenService) {
     this.form = this.formBuilder.group({
       emailAddress: new FormControl([], [Validators.required, Validators.email]),
       password: new FormControl([], [Validators.required])
     })
   }
 
-  ngOnInit(): void { }
+  ngOnInit(): void { 
+    // clear off any tokens in cache
+    // if user lands on login
+    this.tokenService.clearToken();
+  }
 
   login() {
     console.log(this.form.value);
-    this.authService.login(<LoginDTO>this.form.value).subscribe((response) => {
-      this.router.navigate(['items']);
+    this.authService.login(<LoginDTO>this.form.value).subscribe({
+      next: (response) => {
+        this.router.navigate(['items']);
+      },
+      error: (err) => {
+        if (err.status === 401) {
+          this.form.reset();
+          this.errMsg = "Authentication Failed!";
+        }
+        this.isError = true;
+      }
     });
+  }
+
+  back() {
+    this.location.back();
   }
 
 }
--- Client/src/app/providers/config.ts
+++ Client/src/app/providers/config.ts
@@ -1,3 +1,5 @@
+import { environment } from "src/environments/environment";
+
 export class Config {
-    static api: string = "/api/v1";
+    static api: string = environment.apiBaseUri; //"https://localhost:5001/api/v1";
 }
